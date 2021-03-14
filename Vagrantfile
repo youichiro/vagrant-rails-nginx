09e45ebd0251987c094aff7a2ed1c6940b8d6c6b
youichiro <cinnamon416@gmail.com>
2021-03-14 23:20:24 +0900

    fix

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,11 @@
 # -*- mode: ruby -*-
-# vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/focal64"
   config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
   config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.hostname = "test.youichiro.work"
+
+  config.vm.provision :shell, :path => "vagrant/init.sh"
 end
+
---
+++ vagrant/init.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+sudo su
+
+apt-get update
+apt-get -y upgrade
+
+# firewall
+ufw enable
+ufw allow http
+
+# install docker
+apt-get update
+apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release \
+  git
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install docker-ce=5:20.10.5~3-0~ubuntu-focal docker-ce-cli=5:20.10.5~3-0~ubuntu-focal containerd.io
+docker run hello-world
+
+# install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
+# git clone
+mkdir /var/app
+cd /var/app
+
